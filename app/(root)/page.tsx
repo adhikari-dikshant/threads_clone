0c57ade4bd9613b2c6542bd5e76f1ede5f80d1b9
DIKSHANT SINGH ADHIKARI <adhikari.saab03@gmail.com>
2023-11-02 13:43:27 +0530

    home page done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,8 +3,8 @@ import React from "react";
  
 export default function Home() {
   return (
-    <div>
-      <UserButton afterSignOutUrl="/"/>
-    </div>
+    <>
+      <h1 className="head-text text-left">Home</h1>
+    </>
   )
 }
